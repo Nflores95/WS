1139f5c9c5ba2f32f468d49412efc3edaea01142
Nicolas Flores <nflores@inovabiz.com>
2019-11-10 20:44:36 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication2/Controllers/CuotasController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApplication2.Models;
+using System;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+
+namespace WebApplication2.Controllers
+{
+    
+    public class CuotasController : ApiController
+    {
+        conexion _conexion = new conexion();
+        //public CuotasController() { }
+        //public CuotasController(conexion conexion)
+        //{
+        //    this._conexion = conexion ?? throw new ArgumentNullException(nameof(conexion));
+        //}
+        // GET: api/Cuotas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get(int _IdCanal, int _TipoAvance)
+        {
+            var lista_coutas = await _conexion.GetCuotas(_IdCanal, _TipoAvance);
+
+            if(lista_coutas == null)
+            {
+               
+            }
+            return lista_coutas;
+        }
+
+    }
+}
---
+++ WebApplication2/Models/conexion.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace WebApplication2.Models
+{
+    public class conexion
+    {
+        static SqlConnection cn = new SqlConnection();
+        public static async Task AbrirConexionAsync()
+        {
+            cn.ConnectionString = ConfigurationManager.ConnectionStrings["BD_DESARROLLO"].ConnectionString;
+            await cn.OpenAsync();
+        }
+        public static void CerrarConexion()
+        {
+            cn.Close();
+        }
+
+        //static void Main(string[] args)
+        //public async Task<List<string>> GetCuotas()
+        public async Task<List<string>> GetCuotas(int IdCanal, int TipoAvance)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_ObtenerMenuCuotas", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@pIdCanal", IdCanal); //204
+                    cmd.Parameters.AddWithValue("@pTipoAvance", TipoAvance); //101
+                    //cmd.Parameters.Add(new SqlParameter("@pIdCanal",204));
+                    //cmd.Parameters.Add(new SqlParameter("@pTipoAvance",101));
+                    var response = new List<string>();
+                    await AbrirConexionAsync();
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await reader.ReadAsync())
+                        {
+                            response.Add(reader["NumeroOpcion"].ToString()+") "+reader["NumeroCuotas"].ToString() );
+                            Debug.WriteLine("nn"+ reader["NumeroOpcion"].ToString());
+                        }
+                    }
+
+                    return response;
+                }
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
